65dcaae11961223996330495945fb75603ad8a47
Kshitij Kafle <47217907+Kshitij-Kafle-123@users.noreply.github.com>
2022-08-28 12:05:51 +0545

    Updated

--- src/compression/Signature.java
+++ src/compression/Signature.java
@@ -3,7 +3,7 @@ package compression;
 
 public interface Signature {
 	
-	final String huffSignature = "MandeepKshitijBibash";
+	final String huffSignature = "KshitijKafle";
 	final int MAXCHARS = 256;
 	final String strExtension = ".peerTOpeer";
 }
